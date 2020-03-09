63b3c2a84efad8f5887ec620601fc722ac024050
Bhupinder <bhupinder@Macs-MacBook-Pro.local>
2020-03-08 23:09:54 -0400

    Added initial script file

---
+++ myScript.sh
@@ -0,0 +1,12 @@
+# mkdir hello
+# touch "hello/newfile1.txt"
+# echo "I created the new file using my script" >> "hello/newfile1.txt"
+# echo "file created"
+
+open https://facebook.com/
+
+# mkdir hello
+# cd hello
+# touch "newfile3.txt"
+# echo "I created the new file using my script" >> "hello/newfile1.txt"
+# echo "file created"
